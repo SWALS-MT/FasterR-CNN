315421ad9576ec3a810c52a32e71213302fc3aee
SWALS-MT <m.takahashi@sensor.mech.chuo-u.ac.jp>
2021-01-09 15:48:15 +0900

    I added the code for webcam.

---
+++ webcam.py
@@ -0,0 +1,7 @@
+import torch
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+import cv2
+import numpy as np
